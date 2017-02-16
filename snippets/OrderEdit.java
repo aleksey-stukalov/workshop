2f5348413e056a91869ede53f948fac4d83b0846
Olga Shiryaeva <gerlandiya@users.noreply.github.com>
2017-02-16 11:09:04 +0400

    Update OrderEdit.java

--- snippets/OrderEdit.java
+++ snippets/OrderEdit.java
@@ -1,4 +1,15 @@
 public class OrderEdit extends AbstractEditor<Order> {
+
+    @Inject
+    private OrderService orderService;
+
+    @Override
+    protected boolean preCommit() {
+        Order order = getItem();
+        order.setAmount(orderService.calculateAmount(order));
+        return super.preCommit();
+    }
+
     @Override
     protected void initNewItem(Order item) {
         super.initNewItem(item);
